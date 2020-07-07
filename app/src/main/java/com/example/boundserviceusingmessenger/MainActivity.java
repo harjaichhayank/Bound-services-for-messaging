fb1383d14cf9667aae4fac79b10e49d5bd8df0cf
Chhayank <harjaichhayank1@gmail.com>
2020-07-07 15:19:43 +0530

    Context Connection.java is added

--- app/src/main/java/com/example/boundserviceusingmessenger/MainActivity.java
+++ app/src/main/java/com/example/boundserviceusingmessenger/MainActivity.java
@@ -10,11 +10,13 @@ import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
 import android.os.RemoteException;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     Messenger messenger2 = null;
     boolean isBind = false;
@@ -23,18 +25,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "onServiceConnected: should have been called right after onBind: going to service connection");
             messenger2 = new Messenger(service);
             isBind = true;
+            Log.d(TAG, "onServiceConnected: Service is connected");
+            Log.d(TAG, "onServiceConnected: waiting for the next action now");
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "onServiceDisconnected: When am i called?");
             messenger2 = null;
             isBind = false;
         }
@@ -42,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
+        Log.d(TAG, "onStop: Activity is being stopped");
         unbindService(serviceConnection);
         isBind = false;
         messenger2 = null;
@@ -49,33 +55,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void bindService(View view) {
-
         Intent intent = new Intent(this, MyService.class);
+        Log.d(TAG, "bindService: Intent is called");
+        Log.d(TAG, "bindService: bind service will be called and we go to MyService");
         bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+        Log.d(TAG, "bindService: going to onBind now");
     }
 
     public void sayHello(View view) {
         if (isBind) {
             String button_text;
-            button_text = (String) ((Button) view).getText();
+            button_text = ((Button) view).getText().toString();
             if (button_text.equals("Say Hello")) {
-                Message message = Message.obtain(null, MyService.JOB_1, 0, 0, 0);
+                Log.d(TAG, "sayHello: Message is being obtained");
+                Message message = Message.obtain(null, MyService.JOB_1,0,0,0);
+                Log.d(TAG, "sayHello: Message is obtained");
                 try {
                     messenger2.send(message);
+                    Log.d(TAG, "sayHello: Message is send to Incoming Handler in MyService");
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
             } else if (button_text.equals("Say Hello Again")) {
-                Message message = Message.obtain(null, MyService.JOB_2, 0, 0, 0);
+                Log.d(TAG, "sayHello: Message is being obtained");
+                Message message = Message.obtain(null, MyService.JOB_2,0,0,0);
+                Log.d(TAG, "sayHello: Message is obtained");
                 try {
                     messenger2.send(message);
+                    Log.d(TAG, "sayHello: Message is send to Incoming Handler in MyService");
                 } catch (RemoteException e) {
                     e.printStackTrace();
                 }
                 }
             }
         else {
-                Toast.makeText(getApplicationContext(), "Bind Service first", Toast.LENGTH_SHORT).show();
-            }
+            Toast.makeText(getApplicationContext(), "Bind Service first", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "bind First: ");
         }
     }
+}
--- app/src/main/java/com/example/boundserviceusingmessenger/MyService.java
+++ app/src/main/java/com/example/boundserviceusingmessenger/MyService.java
@@ -1,20 +1,21 @@
 package com.example.boundserviceusingmessenger;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
+import android.util.Log;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class MyService extends Service {
 
-    Messenger messenger = new Messenger(new IncomingHandler());
+    private static final String TAG = "MyService";
 
+    Messenger messenger = new Messenger(new IncomingHandler());
     static final int JOB_1 = 1;
     static final int JOB_2 = 2;
 
@@ -22,18 +23,25 @@ public class MyService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         Toast.makeText(getApplicationContext(),"Service is Binding....",Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onBind: onBind is performed + messenger is bounded and sent back");
+        Log.d(TAG, "onBind: going to service connection now");
         return messenger.getBinder();
     }
 
-    class IncomingHandler extends Handler{
+    static class IncomingHandler extends Handler{
+        private ContextConnection contextConnection = new ContextConnection();
+
         @Override
         public void handleMessage(@NonNull Message msg) {
+            Log.d(TAG, "handleMessage: Message is received from MainActivity + checking cases now....");
             switch(msg.what){
                 case JOB_1:
-                    Toast.makeText(getApplicationContext(),"Hello from JOB 1 it is done",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(contextConnection.getMyContext(),"Hello from JOB 1 it is done",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "handleMessage: Hello from JOB 1 it is done");
                     break;
                 case JOB_2:
-                    Toast.makeText(getApplicationContext(),"Hello from JOB 2 it is done",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(contextConnection.getMyContext(),"Hello from JOB 2 it is done",Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "handleMessage: Hello from JOB 2 it is done");
                     break;
                 default:
                     super.handleMessage(msg);
